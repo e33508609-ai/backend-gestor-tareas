0a6924e5a23c7325db49270c968c2e8cd1ec96f4
e33508609 <e33508609@gmail.com>
2026-03-19 13:07:36 -0500

    actualizacion en cors

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://gestor-tareas-chi-six.vercel.app/"],
+  origin: ["http://localhost:5173", "https://gestor-tareas-chi-six.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
